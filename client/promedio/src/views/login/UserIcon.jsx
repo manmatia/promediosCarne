70e6e5d031d43370c32b056999f07151d758a6fb
Matias Graneros <manmatia@gmail.com>
2024-07-06 08:10:22 -0300

    cambios actulualizados

--- client/promedio/src/views/login/UserIcon.jsx
+++ client/promedio/src/views/login/UserIcon.jsx
@@ -12,8 +12,8 @@ const UserIcon = () => {
     <ClerkProvider 
       publishableKey={clerkPubKey} 
       localization={esES}
-      afterSignInUrl="/home"  // URL a la que redirigir después de iniciar sesión
-      afterSignOutUrl="/landing"  // URL a la que redirigir después de cerrar sesión
+      afterSignInUrl="https://promedios-carne.vercel.app/home"  // URL a la que redirigir después de iniciar sesión
+      afterSignOutUrl="https://promedios-carne.vercel.app/landing"  // URL a la que redirigir después de cerrar sesión
     >
       <User />
     </ClerkProvider>
