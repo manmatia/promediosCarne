b9d07e4e808f83afaee2aa9df907a032a4c9ef57
Matias Graneros <manmatia@gmail.com>
2024-07-06 18:03:20 -0300

    domain

--- client/promedio/src/views/landing/Landing.jsx
+++ client/promedio/src/views/landing/Landing.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import "./Landing.css"
 
 function Landing() {
+  const navigate = useNavigate();
+  const navigateTo = (path) => {
+    navigate(path);
+  };
+
   return (
-    <div>landing</div>
+    <div className="landing-container">  <button className="landing-button" onClick={() => navigateTo('https://assured-rhino-35.accounts.dev/sign-in')}>
+ Inicie sesión 
+  </button>
+ </div>
   )
 }
 
