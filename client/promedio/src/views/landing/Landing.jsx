109a779b7c6251a72216d75131c7173d01854805
Matias Graneros <manmatia@gmail.com>
2024-07-06 18:07:37 -0300

    add changes

--- client/promedio/src/views/landing/Landing.jsx
+++ client/promedio/src/views/landing/Landing.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+
 import { Link } from 'react-router-dom';
 import "./Landing.css"
 
 function Landing() {
-  const navigate = useNavigate();
-  const navigateTo = (path) => {
-    navigate(path);
-  };
+
+
 
   return (
     <div>
     <Link to="https://assured-rhino-35.accounts.dev/sign-in">
-    <div className="landing-container">  <button className="landing-button" onClick={() => navigateTo('https://assured-rhino-35.accounts.dev/sign-in')}>
+    <div className="landing-container">  <button className="landing-button" >
  Inicie sesión 
   </button>
  </div>
